7559f63e2d0083e1156773cb6387b7f313ef0482
anniefenton <anniefentoncontact@gmail.com>
2022-03-07 09:08:50 +0000

    arrow functions examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -19,7 +19,24 @@ let sum = addTwoNumbers(3, 7);
 console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid with parenthesis
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers2(6, 9);
+console.log(sum2)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnSMultipleLines());
